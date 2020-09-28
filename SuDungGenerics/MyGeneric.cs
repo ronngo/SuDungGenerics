e11b4957c8b858ad7337d0412b433e96d7db8e72
ronngo <ronngomn23@gmail.com>
2020-09-28 13:56:25 +0700

    first commit

---
+++ SuDungGenerics/MyGeneric.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuDungGenerics
+{
+    public class MyGeneric<T>
+    {
+        public T a;
+        public T b;
+#pragma warning disable CS0693 
+        public void SoSanh<T>(T a, T b)
+
+        {
+            if (a.Equals(b))
+            {
+
+                Console.WriteLine($"{a} bang {b}");
+            }
+            else
+            {
+
+                Console.WriteLine($"{a} khong bang {b}");
+            }
+        }
+    }
+}
